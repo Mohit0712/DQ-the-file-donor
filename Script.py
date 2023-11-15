9ea14445f3fa15b83bdfe503d9e54e2511dc6aaf
Mohit0712 <143384097+Mohit0712@users.noreply.github.com>
2023-11-15 14:31:26 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -188,7 +188,7 @@ Nᴀᴍᴇ - {}"""
 <b>📂 Fɪʟᴇ ɴᴀᴍᴇ : </b> <code>{file_name}</code>
 <b>
 ╭─────── • ◆ • ────────╮
-😰 Horror Movies :  <a href="BhootBazar.online">Tᴏᴜᴄʜ here</a> 👈
+😰 Horror Movies :  <a href="BhootBazar.online">Tᴏᴜᴄʜ here</a> 
 ╰─────── • ◆ • ────────╯
 =========== • ✠ • ===========
 • ᴄʜᴀɴɴᴇʟ : @BhootBazar
